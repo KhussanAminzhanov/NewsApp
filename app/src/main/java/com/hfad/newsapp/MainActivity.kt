59be488083966cf7e3e62fbba71a3656e1442dd6
Khussan Aminzhanov <aminjanovhusan07@gmail.com>
2022-05-08 09:38:15 +0600

    add back button functionality

--- app/src/main/java/com/hfad/newsapp/MainActivity.kt
+++ app/src/main/java/com/hfad/newsapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.hfad.newsapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val backButton = findViewById<Button>(R.id.back_button)
+        val nextButton = findViewById<Button>(R.id.next_button)
+
+        backButton.setOnClickListener {
+            supportFragmentManager.popBackStack()
+        }
     }
 }
